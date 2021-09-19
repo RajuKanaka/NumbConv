6d265a5ac4c0ec1478bc7694c0559cb43341c023
Kanaka Raju <vkanaka.1213@gmail.com>
2021-09-19 20:29:00 +0530

    added script

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+let conFrom = document.getElementById("conFrom").value;
+let conTo = document.getElementById("conTo").value;
+let fromTo = document.getElementById("fromTo");
+console.log(conFrom, conTo, fromTo);
 
+if (conFrom == "Binary" && conTo == "decim") {
+  alert("select proper type");
+}
+if (conFrom == "Binary" && conTo == "Binary") {
+  fromTo.textContent = `${conFrom} to ${conTo}`;
+}
+if (conFrom == "Binary" && conTo == "Binary") {
+  fromTo.textContent = `${conFrom} to ${conTo}`;
+}
+if (conFrom == "Binary" && conTo == "Binary") {
+  fromTo.textContent = `${conFrom} to ${conTo}`;
+}
+if (conFrom == "Binary" && conTo == "Binary") {
+  fromTo.textContent = `${conFrom} to ${conTo}`;
+}
+if (conFrom == "Binary" && conTo == "Binary") {
+  fromTo.textContent = `${conFrom} to ${conTo}`;
+}
+if (conFrom == "Binary" && conTo == "Binary") {
+  fromTo.textContent = `${conFrom} to ${conTo}`;
+}
+if (conFrom == "Binary" && conTo == "Binary") {
+  fromTo.textContent = `${conFrom} to ${conTo}`;
+}
+if (conFrom == "Binary" && conTo == "Binary") {
+  fromTo.textContent = `${conFrom} to ${conTo}`;
+}
+if (conFrom == "Binary" && conTo == "Binary") {
+  fromTo.textContent = `${conFrom} to ${conTo}`;
+}
+if (conFrom == "Binary" && conTo == "Binary") {
+  fromTo.textContent = `${conFrom} to ${conTo}`;
+}
+if (conFrom == "Binary" && conTo == "Binary") {
+  fromTo.textContent = `${conFrom} to ${conTo}`;
+}
